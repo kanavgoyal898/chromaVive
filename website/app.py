aa601d279856c032593e2af616f59838aae22017
Kanav Goyal <kanavgoyal898@gmail.com>
2024-11-05 05:23:24 +0530

    add request methods

--- website/app.py
+++ website/app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request, session
 
 app = Flask(__name__)
+app.secret_key = '0123456789'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return redirect('/result')
+    else:
+        return render_template('index.html')
 
-@app.route('/result')
+@app.route('/result', methods=['GET', 'POST'])
 def result():
-    return render_template('result.html')
+    if request.method == 'POST':
+        return redirect('/result')
+    else:
+        return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
